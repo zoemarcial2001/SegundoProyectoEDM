156de56ae6d560b7189d29223de4d357fd6f9383
Zoe Marcial <Zoe Marcial@LAPTOP-QPF45C5K>
2021-03-26 13:56:36 -0300

    primera parte

---
+++ src/main/java/ar/edu/unju/edm/controler/CalculadoraControler.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.edm.controler;
+
+public class CalculadoraControler {
+
+}
---
+++ src/main/java/ar/edu/unju/edm/model/Calculadora.java
@@ -0,0 +1,63 @@
+package ar.edu.unju.edm.model;
+
+public class Calculadora {
+	private int a;
+	private int b;
+	
+	public Calculadora() {
+		
+	}
+	
+	public int Sumar() {
+		int suma;
+		suma=a+b;
+		return suma;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + a;
+		result = prime * result + b;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Calculadora other = (Calculadora) obj;
+		if (a != other.a)
+			return false;
+		if (b != other.b)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Calculadora [a=" + a + ", b=" + b + "]";
+	}
+
+}
